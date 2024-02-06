ba3e1c748f30b16b7cb395b5390c7b89b162b183
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-06 06:26:50 +0300

    Добавьте файлы проекта.

---
+++ CubeSumSame.cpp
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит целое число А. Программа должна
+// определить, что куб суммы цифр этого числа равен А* А.
+//
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    
+}
