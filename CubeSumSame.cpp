d0ccc1231d5a9771ab21e10f454c5b1a240a0837
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-06 06:39:27 +0300

    Метод вычисляет куб

--- CubeSumSame.cpp
+++ CubeSumSame.cpp
@@ -4,12 +4,36 @@
 
 #include <iostream>
 #include <windows.h>
+#include <cmath>
 using namespace std;
+
+
+int CalcCubSum(int userInput) 
+{
+    int result = 0;
+
+    while (userInput > 0)
+    {
+        int digit = userInput % 10;
+
+        result += digit;
+
+        userInput /= 10;
+    }
+
+    return pow(result, 3); // Или конечно без библиотеки это будет result * result * result
+}
+
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
+    int userInput = 0;
+    cout << "Программа поможет определить что куб суммы цифр этого числа равен А * А, введите число А: ";
+    cin >> userInput;
+
     
+
 }
